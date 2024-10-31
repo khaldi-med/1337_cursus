c55c3dfc1db1424ac525062845b19ececa17d2b3
mohkhald <mohkhald@student.1337.ma>
2024-10-31 01:51:08 +0100

    libft 42 cursus

--- libft.h
+++ libft.h
@@ -5,5 +5,6 @@ int	ft_tolower(int c);
 int	ft_toupper(int c);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:39:31 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/10/31 00:57:11 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/10/31 01:38:57 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@
 
 int	main(void)
 {
+	printf("-->My_func: is %d\n-->Origin: %d", ft_isalnum(126), isdigit(126));
+	/*ft_isdigit();
 	printf("-->My_func: is %d\n-->Origin: %d", ft_isdigit(49), isdigit(49));
-	// ft_isalpha();
-	// printf("-->My_func: is %d\n-->Origin: %d", ft_isalpha(65), isalpha(65));
-	// ft_toupper();
-	/*	printf("-->My_func: is %d\n-->Origin: %d\n", ft_toupper(65),
-			toupper(65));
-		printf("-->My_func: is %c\n-->Origin: %c", ft_toupper(65), toupper(65));
 	*/
-	// ft_tolower();
-	/*
-			printf("-->My_func: is %d\n-->Origin: %d\n", ft_tolower(65),
-			tolower(65));
-			printf("-->My_func: is %c\n-->Origin: %c", ft_tolower(65),
-			tolower(65));
+	/* ft_isalpha();
+		printf("-->My_func: is %d\n-->Origin: %d", ft_isalpha(65), isalpha(65));
+	*/
+	/* ft_toupper();
+		printf("-->My_func: is %d\n-->Origin: %d\n",ft_toupper(65),toupper(65));
+		printf("-->My_func: is %c\n-->Origin: %c", ft_toupper(65), toupper(65));
 		*/
+	/* ft_tolower();
+			printf("-->My_func: is %d\n-->Origin: %d\n",
+				ft_tolower(65),tolower(65));
+			printf("-->My_func: is %c\n-->Origin: %c",
+				ft_tolower(65),tolower(65));
+	*/
 	return (0);
 }
