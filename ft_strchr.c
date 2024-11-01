3e6610983299de5e3e289752751ed54808ab9e56
mohamed <khaldimohamed82@gmail.com>
2024-11-01 16:36:08 +0100

    libft

---
+++ ft_strchr.c
@@ -0,0 +1,13 @@
+#include <libft.h>
+
+char *ft_strchar(const char *str, int c){
+	while(*str != '\0'){
+		if(*str == (char) c)
+			return (char *) str;
+	}
+	str++;
+	if(c == '\0')
+		return (char *) str;
+	return NULL;
+}
+
