ae3909e438ae53bb029205806dbd5c2d730ec5af
mohkhald <mohkhald@student.1337.ma>
2024-11-04 23:49:41 +0100

    libft 42 cursus

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 23:22:48 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/04 23:30:25 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/04 23:46:34 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	s_len;
 	size_t	d_len;
+	size_t	result;
 
 	s_len = ft_strlen(src);
 	d_len = ft_strlen(dst);
+	result = s_len + d_len;
+	if (result >= dstsize)
+		return (0);
 	while (*src != '\0' && *dst != '\0' && s_len <= dstsize - 1)
 	{
 		*(dst + 1) = *src;
@@ -28,5 +32,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst++;
 	}
 	*dst = '\0';
-	return (s_len + d_len);
+	return (result);
 }
