4c91fc7788f980360b825cba6a8257bc9db1fe4f
mohamed <khaldimohamed82@gmail.com>
2024-11-17 11:20:58 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 02:08:36 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/16 06:31:42 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/17 11:20:13 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*src;
-
-	src = (unsigned char *)s;
-	while (n > 0)
-	{
-		*src = 0;
-		src++;
-		n--;
-	}
+	ft_memset(s, 0, n);
 }
