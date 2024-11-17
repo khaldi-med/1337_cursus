d87bb994d0d1868abcaa81e19d46dea1669e5a6c
mohamed <khaldimohamed82@gmail.com>
2024-11-17 13:16:51 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:39:31 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/17 13:05:07 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/17 13:07:50 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,14 @@
 int	main(void)
 {
 	char	*src;
-	char	dest[20];
-	char	des[20];
+	char	*s;
 
 	src = "Hello, World!";
-	ft_memmove(dest, src, 12);
-	memmove(des, src, 12);
-	printf("Dest:%s\n", dest);
-	printf("Des: %s\n", des);
+	s = "Hello, World!";
+
+	ft_memmove(src + 5, src, 5);
+	memmove(s + 5, s, 5);
+	printf("Dest:%s\n", src);
+	printf("Des: %s\n", s);
 	return (0);
 }
