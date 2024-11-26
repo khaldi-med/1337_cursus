0f6e5ec5613bf95ad14cc5cc12d0cff0254e571f
mohamed <khaldimohamed82@gmail.com>
2024-11-26 18:10:59 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 10:54:01 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/26 12:34:42 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/26 18:10:56 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	if (lst == NULL)
+	if (!lst)
 		return (NULL);
 	while (lst->next != NULL)
 		lst = lst->next;
