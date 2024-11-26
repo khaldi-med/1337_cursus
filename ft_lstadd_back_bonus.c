f154c3be9a37fe4b51dea19ee051213e03e79466
mohamed <khaldimohamed82@gmail.com>
2024-11-26 18:23:14 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,15 +6,21 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:20:04 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/26 12:27:50 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/26 18:18:41 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_bonus.h"
 
-/*
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list *end;
+	if(lst == NULL)
+		return;
+	if(lst == NULL){
+		*lst = new;
+		return;
+	}
+	end = ft_lstlast(*lst);
+	end->next = new;
 }
-*
-*/
