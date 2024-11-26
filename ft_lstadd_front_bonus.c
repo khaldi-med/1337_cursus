c7328e13f54e9634c44caac558c162484e0c9779
mohamed <khaldimohamed82@gmail.com>
2024-11-26 18:06:37 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 05:09:22 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/26 10:45:29 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/26 18:06:34 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if(!new)
+	if(!new || !lst)
 		return;
 	new->next = *lst;
 	*lst = new;
