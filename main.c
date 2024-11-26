8f38a296cc3969a9e004511f0018922802191f9e
mohamed <khaldimohamed82@gmail.com>
2024-11-26 12:20:48 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:39:31 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/11/26 11:59:02 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/11/26 12:15:44 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,25 @@
 int	main(void)
 {
 	t_list *head = NULL;
+	//
 	int o = 1;
 	int t = 2;
 	int th = 3;
+	//
 	t_list *one = ft_lstnew(&o);
 	t_list *two = ft_lstnew(&t);
-	t_list *three= ft_lst(&th);
+	t_list *three = ft_lstnew(&th);
+	//
+	ft_lstadd_front(&head, one);
+	ft_lstadd_front(&head, two);
+	ft_lstadd_front(&head, three);
+	//
+	t_list *last = ft_lstlast(head);
+	//
 	printf("%d\n", *(int*)one->content);
 	printf("%d\n", *(int*)two->content);
 	printf("%d\n", *(int*)three->content);
+	printf("%d\n", *(int*)last);
+
 	return (0);
 }
